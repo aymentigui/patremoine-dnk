6175e6af99e89595d3708e14816064ffd4c81fb5
Aymen tigui <aymentigui@gmail.com>
2026-08-18 15:35:47 +0100

    detail mobile page

--- app/(mobile)/app/details/page.tsx
+++ app/(mobile)/app/details/page.tsx
@@ -36,7 +36,7 @@ function DetailsContent() {
   const [emplacements, setEmplacements] = useState<any[]>([]);
 
   useEffect(() => {
-    alert(1)
+    alert(qrCode)
     if (!qrCode) {
       toast.error("Code QR manquant.");
       router.push("/app/home");
